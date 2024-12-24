7f93bcefd789242d8496d4418c5b3d58c87a6a5c
NT0840 <nagata.teruaki.1021@gmail.com>
2024-12-24 15:22:31 +0900

    フィルタの修正、DBファイル配置の修正

--- simpleRoadmapCreator/src/main/java/filter/LoginCheckFilter.java
+++ simpleRoadmapCreator/src/main/java/filter/LoginCheckFilter.java
@@ -10,7 +10,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-@WebFilter("/WEB-INF/jsp/*")
+@WebFilter(servletNames = {"/RoadmapServlet", "/RoadmapTransferServlet", "/AccountServlet", "/AccountTransferServlet"})
 public class LoginCheckFilter extends HttpFilter {
 
 	public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
